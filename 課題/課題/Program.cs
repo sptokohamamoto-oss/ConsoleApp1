8f9defe6585e0e961d501eeeb5518e7c1a6063b9
冬桜子 濱元 <toko0124615@icloud.com>
2026-01-06 15:39:43 +0900

    a

---
+++ 課題/課題/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Security;
+
+class Program
+{
+    static void Main()
+    {
+        int x = 10;
+        int y = 3;
+    
+    console.WriteLine(x + y); 
+        Console.WriteLine(x - y); 
+        Console.WriteLine(x * y); 
+        Console.WriteLine(x / y); 
+
+    }
+}
