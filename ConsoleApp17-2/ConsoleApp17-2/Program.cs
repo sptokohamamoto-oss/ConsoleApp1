b935fa46096379da1208f1b55612aaccf456bbfb
冬桜子 濱元 <toko0124615@icloud.com>
2026-01-14 14:33:03 +0900

    a

---
+++ ConsoleApp17-2/ConsoleApp17-2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        Console.WriteLine("文字列1を入力:");
+        string str1 = Console.ReadLine();
+
+        Console.WriteLine("文字列2を入力:");
+        string str2 = Console.ReadLine();
+
+        Console.WriteLine(str1 + "," + str2);
+
+    }
+
+}
