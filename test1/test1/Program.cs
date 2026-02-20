3ed91493f022257d26a5f3a9b84bde1cb17ddecc
冬桜子 濱元 <toko0124615@icloud.com>
2026-02-20 17:42:38 +0900

    a

---
+++ test1/test1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x = 10;
+            int y = 3;
+
+            Console.WriteLine(x + y); //足し算
+            Console.WriteLine(x - y); //引き算
+            Console.WriteLine(x * y); //掛け算
+            Console.WriteLine(x / y); //割り算
+
+        }
+    }
+}
